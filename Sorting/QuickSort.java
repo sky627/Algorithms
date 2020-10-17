7bf7d4e2012a3904ed2891f39a4e38b40f3c9782
sky627 <55168938+sky627@users.noreply.github.com>
2020-10-16 21:32:45 -0500

    Add files via upload

---
+++ Sorting/QuickSort.java
@@ -0,0 +1,61 @@
+package exercisesInClass;
+
+import java.util.Arrays;
+
+public class QuickSort {
+	/*
+	 * Worst Time Complexity: O(n^2). Worst case occurs when arr is sorted.
+	 * 
+	 * Best and Average Case Time Complexity: O(nlogn).
+	 *
+	 * Auxiliary Space: O(1)
+	 * 
+	 * Stable: No
+	 * 
+	 */
+	public static int[] quickSort(int[] arr) {
+		int[] res = Arrays.copyOf(arr, arr.length);
+		return quickSort(res, 0, arr.length - 1);
+	}
+
+	private static int[] quickSort(int[] arr, int begin, int end) {
+		if (begin < end) {
+			int partition = partition(arr, begin, end);
+			quickSort(arr, begin, partition - 1);
+			quickSort(arr, partition + 1, end);
+		}
+		return arr;
+	}
+
+	private static int partition(int[] arr, int begin, int end) {
+		int pivot = begin;
+		int i = begin + 1, j = end;
+		while (i < j) {
+			while (i < j && arr[i] <= arr[pivot]) {
+				i++;
+			}
+			while (i < j && arr[j] > arr[pivot]) {
+				j--;
+			}
+			swap(arr, i, j);
+		}
+		if (arr[pivot] > arr[i]) {
+			swap(arr, pivot, i);
+			return i;
+		} else {
+			swap(arr, pivot, i - 1);
+			return i - 1;
+		}
+	}
+
+	private static void swap(int[] arr, int i, int j) {
+		int tmp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = tmp;
+	}
+
+	public static void main(String[] args) {
+		int[] arr = { 0, 1, 2, 0 };
+		System.out.println("quickSort:" + Arrays.toString(quickSort(arr)));
+	}
+}
